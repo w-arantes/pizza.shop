696d80f0a861db7e515c1e864aeb06c57900fed2
Wellington Arantes <w.aranteslfc@gmail.com>
2025-02-08 20:00:10 -0300

    feat: enhance dashboard tests to include month metrics and improve visibility checks

--- test/dashboard.spec.ts
+++ test/dashboard.spec.ts
@@ -1,9 +1,30 @@
 import { expect, test } from '@playwright/test'
 
-test('display day orders amount metrics', async ({ page }) => {
+test('display day orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
-  await page.waitForTimeout(2000)
 
-  expect(page.getByText('20', { exact: true })).toBeVisible()
-  expect(page.getByText('20', { exact: true })).toBeVisible()
+  expect(page.getByText('US$ 20,00')).toBeVisible()
+  expect(page.getByText('+1% compared to last month').first()).toBeVisible()
 })
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('20', { exact: true }).first()).toBeVisible()
+  expect(page.locator('div').filter({ hasText: /^20\+1% compared to last month$/ }).getByRole('paragraph')).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('20', { exact: true }).first()).toBeVisible()
+  expect(page.locator('div').filter({ hasText: /^20\+1% compared to last month$/ }).getByRole('paragraph')).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('20', { exact: true }).first()).toBeVisible()
+  expect(page.locator('div').filter({ hasText: /^20\+1% compared to last month$/ }).getByRole('paragraph')).toBeVisible()
+})
+
